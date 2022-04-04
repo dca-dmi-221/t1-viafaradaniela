973b5e16df3e99f0721075fff9c44477af31f7f8
viafaradaniela <1005786523@u.icesi.edu.co>
2022-04-04 12:31:50 -0500

    arreglo

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ function setup() {
   createCanvas(1280, 720);
   slider = createSlider(0, 1, 0.5, 0.01);
   //song = loadSound("music/All_for_us.mp3", loaded)
-  song = loadSound("music/Hit_Em_Up.mp3", loaded)
+  song = loadSound("music/Im_tired.mp3", loaded)
   playlists = loadImage("images/playlist.jpg");
   playlistSeason1 = loadImage("images/playlist_1.jpg");
   playlistSeason2 = loadImage("images/playlist_2.jpg");
@@ -52,24 +52,81 @@ function mousePressed() {
       interface = 1;
     }
   }
-    if (interface === 1) {
-      if (dist(mouseX, mouseY, 452, 141) < 90) {
-        console.log("pantallados");
-        interface = 2;
-      } 
-      
-      else if (dist(mouseX, mouseY, 813, 150) < 90) {
-        interface = 3;
-      } 
-      
-      else if (dist(mouseX, mouseY, 448, 503) < 90) {
-        interface = 4;
-      }
+  if (interface === 1) {
+    if (dist(mouseX, mouseY, 452, 141) < 90) {
+      console.log("pantallados");
+      interface = 2;
+    } else if (dist(mouseX, mouseY, 813, 150) < 90) {
+      interface = 3;
+    } else if (dist(mouseX, mouseY, 448, 503) < 90) {
+      interface = 4;
     }
-    else {
-        if (mouseX > 71 && mouseX < 171 && mouseY > 68 && mouseY < 133) {
-          interface = 1;
-        }
+  } else {
+    if (mouseX > 71 && mouseX < 171 && mouseY > 68 && mouseY < 133) {
+      interface = 1;
     }
   }
+}
 
+
+const songInfo = [{ //playlist season 1
+
+    file: "music/All_for_us.mp3",
+    img: "images/all_for_us.png",
+  },
+  {
+    file: "music/Formula.mp3",
+    img: "images/formula.png",
+  },
+  {
+    file: "music/When_i_r.i.p.mp3",
+    img: "images/when_i_rip.png",
+  },
+  {
+    file: "music/Still_dont_know_my_name.mp3",
+    img: "images/still_dont_know_my_name.png",
+  },
+  {
+    file: "music/Mount_Everest.mp3",
+    img: "images/mount_everest.png",
+  },
+  { //playlist season 2
+
+    file: "music/Im_tired.mp3",
+    img: "images/im_tired.png",
+  },
+  {
+    file: "music/Elliot_song.mp3",
+    img: "images/elliot_song.png",
+  },
+  {
+    file: "music/Pick_Me_Up.mp3",
+    img: "images/pick_me_up.png",
+  },
+  {
+    file: "music/Lo_Vas_A_Olvidar.mp3",
+    img: "images/lo_vas_a_olvidar.png",
+  },
+  {
+    file: "music/Watercolor_eyes.mp3",
+    img: "images/watercolor_eyes.png",
+  },
+  { //playlist fans favorite
+
+    file: "music/Hit_Em_Up.mp3",
+    img: "images/hit_em_up.png",
+
+  },
+  {
+    file: "music/Stand_By_Me.mp3",
+    img: "images/stand_by_me.png",
+  },
+  {
+    file: "music/Yeh_I_Fuckin_Did_It.mp3",
+    img: "images/yeh_i_fuckin_did_it,png",
+  },
+  {
+    file: "music/All_for_us.mp3",
+    img: "images/all_for_us_2.png",
+  }
+]
