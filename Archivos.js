744c8ba191bd48f9e44ec2f2a01ae09e475e3a9c
viafaradaniela <88667483+viafaradaniela@users.noreply.github.com>
2022-04-05 23:33:41 -0500

    ultimos cambios

--- Archivos.js
+++
@@ -1,8 +0,0 @@
-/*class Archivos {
-    constructor(name,song,img){
-        this._name = name;
-        this._song = song
-        this._img = img
-    }
-}
-*/
---
+++ readme.md
@@ -0,0 +1,3 @@
+Nombre: Daniela Viafara Delgado
+Codigo: A00372426
+Link Behance: https://www.behance.net/gallery/141068293/Euphoria-Mp3-Player
